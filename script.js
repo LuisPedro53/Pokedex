74089341a1bc9b98c97c9e842516092e6fdb3d06
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2023-05-20 18:54:08 -0300

    Comando de verificar array

--- script.js
+++ script.js
@@ -1,61 +1,19 @@
 const pegaPokemon = document.getElementById('pegaPokemon');
-mostrarPokemon();
+let pokemons = [];
+// Array que armazenará os nomes que contêm a letra
+const nomesComLetra = [];
+//Primeiro faço uma requisição
+Requisicao();
 
-pegaPokemon.addEventListener('keyup', () => {
-  if (pegaPokemon.value === '') {
-    mostrarPokemon()
-  } else {
-    nomePokemon(pegaPokemon.value);
-  }
-});
+//aqui espero 1 segundo para a requisição acontecer, e apos isso aparece os pokemons na tela todos dentro do array
+setTimeout(() => {
+  mostrarPokemon();
+}, 1500);
 
-function nomePokemon(nome) {
-  fetch('https://pokeapi.co/api/v2/pokemon/' + nome)
-    .then(response => response.json())
-    .then(allPokemon => {
-      let pokemons = [];
-      pokemons.push({
-        nome: allPokemon.name,
-        imagem: allPokemon.sprites.front_default,
-        experiencia:allPokemon.base_experience,
-        altura:allPokemon.height,
-        largura:allPokemon.weight
-      });
-
-      console.log(pokemons);
-
-      let pokemonBoxes = document.querySelector('.pokemon-boxes');
-      pokemonBoxes.innerHTML = "";
-
-      pokemons.map(function (valores) {
-        pokemonBoxes.innerHTML += `
-        <div class="pokemon-box">
-        <div class = "div-nomePokemon">
-          <p class = "nome-pokemon">`+ valores.nome + `</p>
-        </div>
-        <img class = "imagem-pokemon" src="`+ valores.imagem + `"> 
-        <div class = "div-sobrePokemon">
-          <p class = "sobre-pokemon life"> Life: `+ valores.experiencia + `</p>
-          <p class = "sobre-pokemon height"> Altura: `+ valores.altura + `</p>
-          <p class = "sobre-pokemon weight"> Largura: `+ valores.largura + `</p>
-        </div> 
-        </div> 
-        `
-      })
-    })
-    .catch(error => {
-      // Trate o erro aqui sem exibi-lo no console
-      console.log('Ocorreu um erro ao buscar o Pokémon:', error.message);
-    });
-}
-
-
-function mostrarPokemon() {
-
+function Requisicao() {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=52&offset=0')
     .then(response => response.json())
     .then(allPokemon => {
-      let pokemons = [];
 
       allPokemon.results.map((val) => {
         fetch(val.url)
@@ -64,35 +22,125 @@ function mostrarPokemon() {
             pokemons.push({
               nome: val.name,
               imagem: pokemonSingle.sprites.front_default,
-              experiencia:pokemonSingle.base_experience,
-              altura:pokemonSingle.height,
-              largura:pokemonSingle.weight
+              experiencia: pokemonSingle.base_experience,
+              altura: pokemonSingle.height,
+              largura: pokemonSingle.weight,
+              id: pokemonSingle.id
             });
-
-            let pokemonBoxes = document.querySelector('.pokemon-boxes');
-            pokemonBoxes.innerHTML = "";
-
-            pokemons.map(function (valores) {
-              pokemonBoxes.innerHTML += `
-              <div class="pokemon-box">
-              <div class = "div-nomePokemon">
-                <p class = "nome-pokemon">`+ valores.nome + `</p>
-              </div>
-              <img class = "imagem-pokemon" src="`+ valores.imagem + `"> 
-              <div class = "div-sobrePokemon">
-                <p class = "sobre-pokemon" id = "life"> Life: `+ valores.experiencia + ` Hp</p>
-                <p class = "sobre-pokemon" id = "Altura"> Height: `+ valores.altura + ` m</p>
-                <p class = "sobre-pokemon" id = "Largura"> Weight: `+ valores.largura + ` Kg</p>
-              </div> 
-              </div> 
-              `
-            })
           })
       })
     });
 }
 
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  .then(response => response.json())
-  .then(allPokemon => console.log(allPokemon));
+
+function mostrarPokemon() {
+  let loadingContainer = document.querySelector('.loading');
+  let pokemonBoxes = document.querySelector('.pokemon-boxes');
+
+  setTimeout(function () {
+
+    // Tempo de espera para simular o carregamento
+
+    // Resto do seu código para exibir os pokémons  
+    pokemonBoxes.innerHTML = "";
+    pokemons.map(function (valores) {
+      pokemonBoxes.innerHTML += `
+    <div class="pokemon-box">
+      <div class = "div-nomePokemon">
+        <p class = "nome-pokemon">`+ valores.nome + `</p>
+      </div>
+      <img class = "imagem-pokemon" src="`+ valores.imagem + `"  data-id="` + valores.id + `">
+      <div class = "favorita">
+
+      </div> 
+      <div class = "div-sobrePokemon">
+        <p class = "sobre-pokemon" id = "life"> Life: `+ valores.experiencia + ` Hp</p>
+        <p class = "sobre-pokemon" id = "Altura"> Height: `+ valores.altura + ` m</p>
+        <p class = "sobre-pokemon" id = "Largura"> Weight: `+ valores.largura + ` Kg</p>
+      </div> 
+    </div>`;
+    });
+    loadingContainer.style.display = 'none';
+    // Evento de clique da imagem do Pokémon
+    let pokemonsClicados = []; // Array para armazenar os pokémons clicados
+    let badges = document.querySelector(".favoritos-badge");
+    let imagens = document.querySelectorAll('.imagem-pokemon');
+    imagens.forEach(function (imagem) {
+      imagem.addEventListener('click', function () {
+        // Encontrar o objeto do pokémon clicado com base no ID
+        var pokemonObj = pokemons.find(function (pokemon) {
+          return pokemon.id === parseInt(this.dataset.id);
+        }, this);
+
+        // Verificar se o Pokémon já está no array
+        var isDuplicate = pokemonsClicados.some(function (pokemon) {
+          return pokemon.id === pokemonObj.id;
+        });
+
+        if (!isDuplicate) {
+          pokemonsClicados.push(pokemonObj);
+        }
+        // Adicionar o objeto do pokémon ao array
+
+
+        console.log('Pokémons clicados:', pokemonsClicados); // Exibir no console os pokémons clicados
+        badges.style.display = 'inline-block';
+        badges.innerHTML = pokemonsClicados.length;
+      });
+    });
+  }, 2000);
+}
+
+
+//aqui a cada tecla precionda no campo, é feita uma pesquisa no array, e caso aquela letra for encontrada
+//é adicionado na tela os pokemons com aquelas letras
+pegaPokemon.addEventListener('keyup', () => {
+  if (pegaPokemon.value === '') {
+    mostrarPokemon()
+    nomesComLetra.length = 0;
+  } else {
+    // nomePokemon(pegaPokemon.value);
+    verificarPokemon(pegaPokemon.value);
+  }
+});
+
+
+// Função de keyup
+function verificarPokemon(letra) {
+  // Filtrar os nomes que contêm a letra e adicioná-los ao novo array
+  nomesComLetra.length = 0; // Limpa o array antes de cada verificação
+  pokemons.filter(p => {
+    if (p.nome.includes(letra)) {
+      nomesComLetra.push(p);
+
+      let pokemonBoxes = document.querySelector('.pokemon-boxes');
+      pokemonBoxes.innerHTML = "";
+
+      nomesComLetra.map(function (valores) {
+        pokemonBoxes.innerHTML += `
+        <div class="pokemon-box">
+          <div class = "div-nomePokemon">
+            <p class = "nome-pokemon">`+ valores.nome + `</p>
+          </div>
+          <img class = "imagem-pokemon" src="`+ valores.imagem + `"> 
+          <div>
+            
+          </div>
+          <div class = "div-sobrePokemon">
+          <p class = "sobre-pokemon" id = "life"> Life: `+ valores.experiencia + ` Hp</p>
+          <p class = "sobre-pokemon" id = "Altura"> Height: `+ valores.altura + ` m</p>
+          <p class = "sobre-pokemon" id = "Largura"> Weight: `+ valores.largura + ` Kg</p>
+         </div> 
+        </div> 
+        `
+      })
+    }
+  });
+}
+
+
+
+
+
+
 
